4485c7f04d8c3b6042f3d56a59290c16375b8e30
Cody Um <yunsuum@gmail.com>
2020-08-09 18:30:48 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,6 @@ import Detail from './Components/Detail/detail';
 
 import PostListing from './Components/PostListing/postListing'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-<<<<<<< HEAD
-=======
-
->>>>>>> master
-
 
 function App() {
   return (
